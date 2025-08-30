38da43aa5c96d9b9872f2fd533d906024de8c43a
Freddie <fredherbert47@outlook.com>
2025-08-31 00:10:21 +0100

    Fix socket constructor for ngx-socket-io compatibility

--- src/app/wavoracle-socket.ts
+++ src/app/wavoracle-socket.ts
@@ -6,6 +6,6 @@ export class WavOracleSocket extends Socket {
   constructor() {
     const path =
       document.location.pathname.replace(/share-target/, '') + 'socket.io';
-    super({ url: '', options: { path } });
+    super({ url: '', options: { path } }, null);
   }
 }
