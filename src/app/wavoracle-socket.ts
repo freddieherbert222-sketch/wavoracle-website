55ebaa98d80532740420fdbf32d3647cf039db7e
Freddie <fredherbert47@outlook.com>
2025-08-31 01:16:44 +0100

    Fix socket dependency: replace ngx-socket-io with socket.io-client for Angular 17.3.x compatibility

--- src/app/wavoracle-socket.ts
+++ src/app/wavoracle-socket.ts
@@ -1,11 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class WavOracleSocket extends Socket {
+export class WavOracleSocket {
+  private socket: Socket;
+
   constructor() {
-    const path =
-      document.location.pathname.replace(/share-target/, '') + 'socket.io';
-    super({ url: '', options: { path } }, null);
+    const path = document.location.pathname.replace(/share-target/, '') + 'socket.io';
+    this.socket = io('', { path });
+  }
+
+  // Add the fromEvent method that your downloads service needs
+  fromEvent(event: string) {
+    return new Observable((observer) => {
+      this.socket.on(event, (data) => {
+        observer.next(data);
+      });
+      
+      // Return cleanup function
+      return () => {
+        this.socket.off(event);
+      };
+    });
+  }
+
+  // Other socket methods
+  connect() {
+    this.socket.connect();
+  }
+
+  disconnect() {
+    this.socket.disconnect();
+  }
+
+  emit(event: string, data: any) {
+    this.socket.emit(event, data);
+  }
+
+  on(event: string, callback: (data: any) => void) {
+    this.socket.on(event, callback);
+  }
+
+  getSocket(): Socket {
+    return this.socket;
   }
 }
